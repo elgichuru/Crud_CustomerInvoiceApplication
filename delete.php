8821487de5347876021baa735ede5c5260444cb6
Mutwiri <41791329+elgichuru@users.noreply.github.com>
2024-04-24 11:34:05 +0300

    Add files via upload

---
+++ delete.php
@@ -0,0 +1,15 @@
+<?php
+require 'database.php';
+
+$product_id=filter_input(INPUT_POST,'product_id',FILTER_VALIDATE_INT);
+$category_id=filter_input(INPUT_POST,'category_id',FILTER_VALIDATE_INT);
+//delete from the database
+if($product_id !=false && $category_id!=false){
+$query='DELETE FROM products where productID=:productID';
+
+$statement=$db->prepare($query);
+$statement->bindvalue(':productID',$product_id);
+$success=$statement->execute();
+$statement->closecursor();
+
+}
